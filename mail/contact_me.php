c5040c270831ae272a7a6193cdc6d04889ff5b43
Nikolas Martens <Nikolas.M@rtens.org>
2016-03-03 15:19:05 +0600

    Fixed syntax error

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 $to = 'contact@groupcash.org';
-$email_subject = [groupcash.org] Contact Form:  $name";
+$email_subject = "[groupcash.org] Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@groupcash.org\n";
 $headers .= "Reply-To: $email_address";
